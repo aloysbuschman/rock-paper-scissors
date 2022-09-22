31904d517eef579c1c1922a35867835b656fae1b
aloysbuschman <aloys.buschman@gmail.com>
2022-09-22 15:05:49 +0200

    Add playRound() and variables for win / lose

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
+const results = {
+    win: "You win!",
+    lose: "You lose",
+    tie: "It is a tie"
+}
+const playerSelectedRock = {
+    rock: results.tie,
+    paper: results.lose,
+    scissors: results.win
+}
+
+const playerSelectedPaper = {
+    rock: results.win,
+    paper: results.tie,
+    scissors: results.lose
+}
+
+const playerSelectedScissors = {
+    rock: results.lose,
+    paper: results.win,
+    scissors: results.tie
+}
+
 function getComputerChoice() {
-    const options = ['Rock', 'Paper', 'Scissors']
+    const options = ["rock", "paper", "scissors"]
     const selectedOption = options[Math.floor(Math.random() * 3)]
-    console.log(selectedOption);
+    return selectedOption;
 }
 
 getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    console.log({playerSelection}, {computerSelection})
+    switch (playerSelection) {
+        case "rock": 
+            console.log(playerSelectedRock[computerSelection])
+            break;
+        case "paper": 
+            console.log(playerSelectedPaper[computerSelection])
+            break;
+        case "scissors": 
+            console.log(playerSelectedScissors[computerSelection])
+            break;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
