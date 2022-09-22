51295ce32e1d2a4e6381a6ceebbece6fc150a968
aloysbuschman <aloys.buschman@gmail.com>
2022-09-22 10:33:34 +0200

    Add getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Does this work?')
+function getComputerChoice() {
+    const options = ['Rock', 'Paper', 'Scissors']
+    const selectedOption = options[Math.floor(Math.random() * 3)]
+    console.log(selectedOption);
+}
+
+getComputerChoice();
